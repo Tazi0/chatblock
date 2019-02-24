96f8cd405dc7a15def16f5522b35eeaf920650e6
Tazio <45716542+Tazi0@users.noreply.github.com>
2019-02-24 19:19:45 +0100

    V1.1

--- chatblock/main.lua
+++ chatblock/main.lua
@@ -2,6 +2,13 @@
 
 AddEventHandler('chatMessage', function(source, name, msg)
 
+  if string.match(msg, "[12]?%d%d?%.[12]?%d%d?%.[12]?%d%d?%.[12]?%d%d?") and string.match(msg, "https?://[%w-_%.%?%.:/%+=&]+") then -- IP address
+		CancelEvent()
+    msg = msg:gsub("https?://[%w-_%.%?%.:/%+=&]+", "blocked_link")
+		msg = msg:gsub("[12]?%d%d?%.[12]?%d%d?%.[12]?%d%d?%.[12]?%d%d?", "blocked_ip")
+		TriggerClientEvent('chatMessage', -1, name, { 255, 255, 255 }, msg)
+  end
+
   if string.match(msg, "[12]?%d%d?%.[12]?%d%d?%.[12]?%d%d?%.[12]?%d%d?") then -- IP address
 		CancelEvent()
 		msg = msg:gsub("[12]?%d%d?%.[12]?%d%d?%.[12]?%d%d?%.[12]?%d%d?", "blocked_ip")
